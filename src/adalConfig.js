87e1ad1514ad2fe573cb39f9aecf3260f55ece1e
Kamil Janowski <kamil.janowski@f-secure.com>
2019-06-01 17:39:44 +0300

    more adal config

--- src/adalConfig.js
+++ src/adalConfig.js
@@ -5,7 +5,7 @@ export const adalConfig = {
   clientId: '8beba41e-e4b3-4e8a-b694-982522d53a3a',//process.env.REACT_APP_AAD_CLIENT_ID,
   postLogoutRedirectUri: window.location.origin,
   endpoints: {
-    api: 'https://graph.microsoft.com'//process.env.REACT_APP_OBJECT_ID
+    api: 'https://login.microsoftonline.com/d5c08352-b4fe-4bbe-97c8-5f577bd03063/oauth2/v2.0/authorize'//process.env.REACT_APP_OBJECT_ID
   },
   cacheLocation: 'localStorage',
 };
